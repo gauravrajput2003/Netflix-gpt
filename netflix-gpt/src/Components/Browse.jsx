eef99c5ea409d39179b757e5104bc222c9e6ae73
gaurav <gauravkumarsingh010103@gmail.com>
2025-01-14 18:12:05 +0530

    all movies section added

--- netflix-gpt/src/Components/Browse.jsx
+++ netflix-gpt/src/Components/Browse.jsx
@@ -3,7 +3,7 @@ import Header from './Header';
 import useNowPlaying from '../hooks/useNowPlaying';
 import Maincontainer from './maincontainer';
 import Secondarycontainer from './Secondarycontainer';
-import usePopularMovies from '../hooks/usetopratedMovies';
+import usePopularMovies from '../hooks/usePopularMovies';
 import usetopratedMovies from '../hooks/usetopratedMovies';
 import useUpcomig from '../hooks/useUpcomig';
 
